656b4d40f451125b5b72c365900982ce7c7d96d6
Rafael Barbosa <rsbarbosa0802@gmail.com>
2024-04-17 10:36:58 -0300

    Curso C# - Aula sobre interface

---
+++ Course/Services/BrazilTaxService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Course.Services
+{
+    class BrazilTaxService : ITaxService //realização de interface e nao herança.
+    {
+        public double Tax(double amount)
+        {
+            if (amount <= 100.0)
+            {
+                return amount * 0.2;
+            }
+            else
+            {
+                return amount * 0.15;
+            }
+        }
+    }
+}
---
+++ Course/Services/ITaxService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Course.Services
+{
+    interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
